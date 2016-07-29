64c6f4cc1f6a7955cf448010a89d3473974abc05
Petro Galushchak <petrogalushchak@gmail.com>
2016-07-29 15:31:00 +0300

    Final fixes

--- templates/host_updater.sh
+++ templates/host_updater.sh
@@ -15,7 +15,7 @@ while test -n "$1"; do
   shift
 done
 
-function check_author_ip {
+check_author_ip() {
 OLD_IP=$(cat /etc/hosts | grep author | awk '{print $1}')
 NEW_IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$ENV-author" | grep "PrivateIpAddress" | awk 'NR==1' | awk '{gsub(/"/, "", $2); print $2}' | sed 's/,//g')
 if [ $OLD_IP != $NEW_IP ]
@@ -25,42 +25,46 @@ then
 fi
 }
 
-function check_publisher_ip {
-OLD_IP=$(cat /etc/hosts | grep publisher$1 | awk '{print $1}')
-NEW_IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$ENV-publisher$1" | grep "PrivateIpAddress" | awk 'NR==1' | awk '{gsub(/"/, "", $2); print $2}' | sed 's/,//g')
+check_publisher_ip() {
+PUB_N=$1
+OLD_IP=$(cat /etc/hosts | grep publisher$PUB_N | awk 'NR==1{print $1}' | awk '{print $1}')
+NEW_IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$ENV-publisher$PUB_N" | grep "PrivateIpAddress" | awk 'NR==1' | awk '{gsub(/"/, "", $2); print $2}' | sed 's/,//g')
 if [ $OLD_IP != $NEW_IP ]
 then
-	echo "Setting new IP for publisher"
+	echo "Setting new IP for publisher$PUB_N"
 	sed -i -- "s/$OLD_IP/$NEW_IP/g" /etc/hosts && echo "IP set"
 fi
 }
 
-function check_prepublisher_ip {
-OLD_IP=$(cat /etc/hosts | grep prepublisher$1 | awk '{print $1}')
-NEW_IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$ENV-prepublisher$1" | grep "PrivateIpAddress" | awk 'NR==1' | awk '{gsub(/"/, "", $2); print $2}' | sed 's/,//g')
+check_prepublisher_ip() {
+PUB_N=$1
+OLD_IP=$(cat /etc/hosts | grep prepublisher$PUB_N | awk 'NR==1{print $1}' | awk '{print $1}')
+NEW_IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=pre$ENV-publisher$PUB_N" | grep "PrivateIpAddress" | awk 'NR==1' | awk '{gsub(/"/, "", $2); print $2}' | sed 's/,//g')
 if [ $OLD_IP != $NEW_IP ]
 then
-	echo "Setting new IP for publisher"
+	echo "Setting new IP for prepublisher$PUB_N"
 	sed -i -- "s/$OLD_IP/$NEW_IP/g" /etc/hosts && echo "IP set"
 fi
 }
 
-function check_dispatcher_ip {
-OLD_IP=$(cat /etc/hosts | grep dispatcher$1 | awk '{print $1}')
-NEW_IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$ENV-dispatcher$1" | grep "PrivateIpAddress" | awk 'NR==1' | awk '{gsub(/"/, "", $2); print $2}' | sed 's/,//g')
+check_dispatcher_ip() {
+DISP_N=$1
+OLD_IP=$(cat /etc/hosts | grep dispatcher$DISP_N | awk 'NR==1{print $1}' | awk '{print $1}')
+NEW_IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$ENV-dispatcher$DISP_N" | grep "PrivateIpAddress" | awk 'NR==1' | awk '{gsub(/"/, "", $2); print $2}' | sed 's/,//g')
 if [ $OLD_IP != $NEW_IP ]
 then
-	echo "Setting new IP for dispatcher"
+	echo "Setting new IP for dispatcher$DISP_N"
 	sed -i -- "s/$OLD_IP/$NEW_IP/g" /etc/hosts && echo "IP set"
 fi
 }
 
-function check_predispatcher_ip {
-OLD_IP=$(cat /etc/hosts | grep predispatcher$1 | awk '{print $1}')
-NEW_IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$ENV-predispatcher$1" | grep "PrivateIpAddress" | awk 'NR==1' | awk '{gsub(/"/, "", $2); print $2}' | sed 's/,//g')
+check_predispatcher_ip() {
+DISP_N=$1
+OLD_IP=$(cat /etc/hosts | grep predispatcher$DISP_N | awk 'NR==1{print $1}' | awk '{print $1}')
+NEW_IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=pre$ENV-dispatcher$DISP_N" | grep "PrivateIpAddress" | awk 'NR==1' | awk '{gsub(/"/, "", $2); print $2}' | sed 's/,//g')
 if [ $OLD_IP != $NEW_IP ]
 then
-	echo "Setting new IP for dispatcher"
+	echo "Setting new IP for predispatcher$DISP_N"
 	sed -i -- "s/$OLD_IP/$NEW_IP/g" /etc/hosts && echo "IP set"
 fi
 }
@@ -93,15 +97,18 @@ function check_prod {
 
 check_author_ip
 
-if [ "$ENV" == "dev"] then
-	check_publisher_ip 1
-	check_dispatcher_ip 1
+if [ $ENV == dev ]
+then
+	check_publisher_ip
+	check_dispatcher_ip
 fi
 
-if [ "$ENV" == "stage"] then
+if [ $ENV == stage ]
+then
   check_stage
 fi
 
-if [ "$ENV" == "prod"] then
+if [ $ENV == prod ]
+then
   check_prod
 fi
